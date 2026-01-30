d759cef8ce634dda93d24313230c12d721e8c770
Louis Alexandre Richoux <louisalex.richoux@gmail.com>
2026-01-30 23:15:46 +0100

    new version more SaaS

--- app/controllers/concerns/plan_enforceable.rb
+++ app/controllers/concerns/plan_enforceable.rb
@@ -6,32 +6,32 @@ module PlanEnforceable
   def enforce_lecture_limit!
     return true if current_user.can_create_lecture?
 
-    redirect_to lectures_path,
-      alert: "Vous avez atteint la limite de #{current_user.plan_limit(:max_lectures)} cours pour le plan gratuit. Passez a Studigo Pro pour un acces illimite !"
+    redirect_to pricing_path,
+      alert: "Tu as atteint la limite de #{current_user.plan_limit(:max_lectures)} cours du plan gratuit. Passe à Studigo Pro pour un accès illimité."
     false
   end
 
   def enforce_message_limit!
     return true if current_user.can_send_message?
 
-    redirect_back fallback_location: lectures_path,
-      alert: "Vous avez atteint la limite de #{current_user.plan_limit(:max_messages_total)} messages au total. Passez a Studigo Pro pour un acces illimite !"
+    redirect_to pricing_path,
+      alert: "Tu as utilisé tes #{current_user.plan_limit(:max_messages_total)} messages IA du plan gratuit. Passe à Studigo Pro pour un accès illimité."
     false
   end
 
   def enforce_flashcard_generation_limit!
     return true if current_user.can_generate_flashcards?
 
-    redirect_back fallback_location: lectures_path,
-      alert: "Vous avez atteint la limite de #{current_user.plan_limit(:max_flashcard_generations)} generations de flashcards. Passez a Studigo Pro pour un acces illimite !"
+    redirect_to pricing_path,
+      alert: "Tu as utilisé tes #{current_user.plan_limit(:max_flashcard_generations)} générations de flashcards du plan gratuit. Passe à Studigo Pro pour un accès illimité."
     false
   end
 
   def enforce_quiz_generation_limit!
     return true if current_user.can_generate_quiz?
 
-    redirect_back fallback_location: quizzes_path,
-      alert: "Vous avez atteint la limite de #{current_user.plan_limit(:max_quiz_generations)} creations de quiz. Passez a Studigo Pro pour un acces illimite !"
+    redirect_to pricing_path,
+      alert: "Tu as utilisé ta #{current_user.plan_limit(:max_quiz_generations)} création de quiz du plan gratuit. Passe à Studigo Pro pour un accès illimité."
     false
   end
 
@@ -39,7 +39,7 @@ module PlanEnforceable
     return true if current_user.has_multiplayer_access?
 
     redirect_to pricing_path,
-      alert: "Le mode multijoueur est reserve aux abonnes Studigo Pro."
+      alert: "Le mode multijoueur est réservé aux abonnés Studigo Pro."
     false
   end
 end
